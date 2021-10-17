ab949ca8051f909734476998afdc6529ac7fddbd
jay-web <jay.developer12@gmail.com>
2021-10-17 18:34:18 +0530

    text change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import {withAuthenticator, AmplifySignOut} from "@aws-amplify/ui-react";
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-        <h1>We now have Auth!</h1>
+        <h1>We now have Authentication!</h1>
       </header>
       <AmplifySignOut />
     </div>
